e520b7ce4717d1a3cd7923b729d6a7f9f911358d
pystub <pystub@gmail.com>
2013-07-21 17:12:58 +0300

    rewrite

--- bot.py
+++ bot.py
@@ -1,110 +1,119 @@
-import ch
+import ch, re, json
 import threading
-import tkinter
 import time
 import webbrowser
-from tkinter import *
+
+import tkinter as tk
+try:
+  from tkinter import ttk
+  spawner = tk.ttk
+except:
+  spawner = tk
+
+with open ('settings.json') as f:
+  settings = json.loads (f.read ())
+except:
+  raise Exception ('settings.json file is missing')
 
 class TestBot(ch.RoomManager):
-  def onConnect(self, room):
-    if NICK != "":
-      self.setNameColor(Settings[5])
-      self.setFontColor(Settings[6])
-      self.setFontFace(Settings[7])
-      self.setFontSize(int(Settings[8]))
-    else:
-      self.setNameColor("333333")
-      self.setFontColor("FFFFFF")
-      self.setFontFace("Latha")
-      self.setFontSize(11)    
-    self.ListNum = 0
-    listbox.insert(END, 'Connected to '+room.name+'\n')
+  def onConnect (self, room):
+    self.setNameColor ("333333")
+    self.setFontColor ("FFFFFF")
+    self.setFontFace ("Latha")
+    self.setFontSize (11)
+    self.gui.post ('connected to {}'.format (room.name))
 
+  def onMessage (self, room, user, message):
+    if user.name not in settings['ignored']:
+      self.gui.post ('{}: {}'.format (user.name, message.body))
 
-  def onMessage(self, room, user, message):
-    if user.name not in Ignored:
-      self.ListNum += 1
-      listbox.insert(END, user.name+'-'+time.strftime('%X')+'   '+message.body+'\n')
-      listbox.itemconfig(self.ListNum,fg='#'+user.fontColor)
-      listbox.yview_scroll(self.ListNum, 'p')
-Ignored = []
+class BotGUI (tk.Frame):
+  def __init__ (self, master = None):
+    spawner.Frame.__init__ (self, master)
+    self.pack (fill = tk.BOTH, expand = True)
+    self.createWidgets ()
 
-try:
-  with open ('settings.txt', 'r') as f: 
-    Settings=f.read().split('|')
-    print (Settings)
-    NICK= Settings[0]
-    PASS = Settings[1]
-    ROOM = Settings[2]
-    Ignored = Settings[3]
-except IOError:
-  print ("Missing 'Settings.txt' file")
+    self.bot = TestBot (settings['name'], settings['password'])
+    self.bot.gui = self
+    self.bot.joinRoom (settings['room'])
+    self.botThread = threading.Thread (target = self.bot.main,)
+    self.botThread.start ()
 
-if NICK != "":
-  TestBot = TestBot(NICK,PASS)
-  TestBot.joinRoom(ROOM)
-else:
-  NICK = input("Your Account name: ")
-  PASS = input("Your Password: ")
-  ROOM = input("Room name: ")
-  TestBot = TestBot(NICK,PASS)
-  TestBot.joinRoom(ROOM)
-  Settings = [NICK, PASS, ROOM, ' ', '999', '9c2afc', '2da446', '0', '11']
-  with open ('settings.txt', 'w') as f: f.write(NICK+'|'+PASS+'|'+ROOM+'| |999|9c2afc|2da446|0|11')
-
-TestBot_thread = threading.Thread(target=TestBot.main,)
-TestBot_thread.setDaemon(True)
-TestBot_thread.start()
-
-class BotGUI(tkinter.Tk):
-
-  def Select(event):
+  def Select (event):
     if 'http' in '\n'.join([listbox.get(x) for x in listbox.curselection()]):
       for i in '\n'.join([listbox.get(x) for x in listbox.curselection()]).split():
         if 'http' in i:
           webbrowser.open(i)
           return #Return added so only uses one link in message
 
-  def get(event):
-    try:
-      if '!bg' in text.get(): 
-        if text.get().split()[1].isdigit():
-          listbox.config(bg='#'+str(text.get().split()[1]))
-        else:
-          listbox.config(bg=str(text.get().split()[1]))
-      elif '!nc' in text.get():
-        TestBot.setNameColor(text.get().split()[1])
-      elif '!bc' in text.get():
-        TestBot.setFontColor(text.get().split()[1])
-      elif '!bs' in text.get():
-        TestBot.setFontSize(int(text.get().split()[1]))
-      elif '!font' in text.get():
-        TestBot.setFontFace(text.get().split()[1])
-      elif '!ignore' in text.get():
-        if text.get().split()[1] in TestBot.Ignored:
-          TestBot.Ignored += text.get().split()[1]
-        else:
-          TestBot.Ignored = TestBot.Ignored.replace(text.get().split()[1],"")
-      else:
-        room = TestBot.getRoom(ROOM)
-        room.message(text.get(), False)  
-    except Exception as error: print (error)
-    text.delete(0,20000) #Clears Textfield
-  
-  GUI = tkinter.Tk()
-  GUI.title('Chat Client')
-  GUI.geometry("360x660")
-  GUI.minsize(100,100)
-  GUI.wm_attributes("-topmost", 1)
-  global listbox,text
-  text = Entry(GUI)
-  text.bind('<Return>', get)
-  listbox = Listbox(GUI)
-  listbox.bind('<<ListboxSelect>>',Select)
-  text.pack(fill=BOTH, side='bottom')
-  listbox.config(bg='#'+Settings[4], borderwidth=0, selectborderwidth=0, height=20)
-  listbox.pack(side='right',fill=BOTH, expand=1)
+  def get (self, event):
+    message = self.text.get ()
+    post = False
+    r = re.match (r'^!(\w+) (.+)', message)
+    if not r:
+      post = True
+    elif r.group (1) == 'bg':
+      self.helperCanvas.config (bg = r.group (2))
+    elif r.group (1) == 'nc':
+      pass
+    elif r.group (1) == 'bc':
+      pass
+    elif r.group (1) == 'bs':
+      pass
+    elif r.group (1) == 'font':
+      pass
+    elif r.group (1) == 'ignore':
+      pass
+    else:
+      post = True
 
-gui_thread = threading.Thread(target=tkinter.mainloop(),)
-gui_thread.setDaemon(True)
-gui_thread.start()
+    if post:
+      self.bot.getRoom (settings['room']).message (message, False)
+
+    self.text.delete (0,20000) #Clears Textfield
+
+  def post (self, message):
+    w = self.helperCanvas.winfo_width ()
+    newEntry = spawner.Label (self.messageStream, text = message, justify = tk.LEFT, width = w, wraplength = w)
+    newEntry.pack (side = tk.TOP)
+    self.messageStream.update_idletasks ()
+    self.helperCanvas.configure (scrollregion = (0, 0, self.messageStream.winfo_width (), self.messageStream.winfo_height ()))
+
+  def createWidgets (self):
+    self.messageFrame = spawner.Frame (self)
+    self.messageFrame.pack (side = tk.TOP, fill = tk.BOTH, expand = True)
+    self.messageFrame.grid_rowconfigure (0, weight = 1)
+    self.messageFrame.grid_columnconfigure (0, weight = 1)
+
+    self.helperCanvas = tk.Canvas (self.messageFrame)
+    self.helperCanvas.grid (row = 0, column = 0, sticky = tk.NSEW)
+
+    vsb = spawner.Scrollbar (self.messageFrame, orient = tk.VERTICAL, command = self.helperCanvas.yview)
+    vsb.grid (row = 0, column = 1, sticky = tk.NS)
+    self.helperCanvas.configure (yscrollcommand = vsb.set)
+
+    self.messageStream = spawner.Frame (self.helperCanvas)
+    self.helperCanvas.create_window (0, 0, window = self.messageStream, anchor = tk.NW)
+
+    self.messageStream.update_idletasks ()
+    self.helperCanvas.configure (scrollregion = (0, 0, self.messageStream.winfo_width (), self.messageStream.winfo_height ()))
+
+    #listbox = Listbox (root)
+    #listbox.bind ('<<ListboxSelect>>',Select)
+    self.text = spawner.Entry (root, width = '0')
+    self.text.pack (side = tk.LEFT, fill = tk.X, expand = True)
+    self.text.bind ('<Return>', self.get)
+    #listbox.config (bg='#484848', borderwidth=0, selectborderwidth=0)
+    #listbox.pack (side='right', fill=BOTH, expand=1)
+    self.post ('test')
+
+root = tk.Tk ()
+root.title ('Chat Client')
+root.geometry ("360x660")
+root.minsize (100,100)
+gui = BotGUI (master = root)
+gui.mainloop ()
+
+# gui_thread = threading.Thread (target = gui.mainloop,)
+# gui_thread.setDaemon (True)
+# gui_thread.start ()
